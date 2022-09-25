a6691093b367bdab9e1d661da1db5cd20b0c8b56
Adilson2902 <nunojraa@hotmail.com>
2022-09-25 11:40:05 -0300

    configurando o prismic

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,9 @@
-export default function Header() {
-  // TODO
+import styles from './header.module.scss';
+
+export default function Header(): JSX.Element {
+  return (
+    <div className={styles.containerheader}>
+      <img src="/assets/Logo.svg" alt="logo" />
+    </div>
+  );
 }
