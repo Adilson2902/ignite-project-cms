3882ebfe69c0bf764ba8c296b5b882f739faffd2
Adilson2902 <nunojraa@hotmail.com>
2022-09-27 19:22:43 -0300

    ajustes na config do posts

---
+++ .slicemachine/assets/slices/Post/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/Post';
+
+export default {
+  title: 'slices/Post'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"title":[{"type":"heading1","text":"Leaving","spans":[]}],"description":[{"type":"paragraph","text":"Duis cillum do sint deserunt quis fugiat ut sunt incididunt anim. Est aute cupidatat ipsum sit pariatur eu ad.","spans":[]}]},"slice_type":"post","id":"_Default"}} />
+_Default.storyName = ''
