93a2d31fa0e9843ea10120cbbfebd21dfd04f961
adilson2902 <nunojraa@hotmail.com>
2022-12-07 11:34:06 -0300

    Ajutes no blog

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,7 +3,9 @@ import styles from './header.module.scss';
 export default function Header(): JSX.Element {
   return (
     <div className={styles.containerheader}>
-      <img src="/assets/Logo.svg" alt="logo" />
+      <div className={styles.background_img}>
+        <img src="/assets/Logo.svg" alt="logo" width={238} height={25} />
+      </div>
     </div>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,33 @@
 import { GetStaticProps } from 'next';
+import Head from 'next/head';
+import Link from 'next/link';
+import { RichText } from 'prismic-dom';
 
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
 
+interface Content {
+  type: string;
+  text: string;
+  span: any;
+}
+
 interface Post {
+  slug?: string;
   uid?: string;
   first_publication_date: string | null;
   data: {
     title: string;
     subtitle: string;
     author: string;
+    content: Content;
   };
 }
 
 interface PostPagination {
-  next_page: string;
+  next_page?: string;
   results: Post[];
 }
 
@@ -24,13 +35,62 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-export default function Home(): JSX.Element {
-  return <h1>Oi</h1>;
+export default function Home(props: HomeProps): JSX.Element {
+  // eslint-disable-next-line react/destructuring-assignment
+  const { results } = props.postsPagination;
+
+  return (
+    <>
+      <Head>
+        <title>Posts | Desafio Ignite</title>
+      </Head>
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          {results.map(i => {
+            return (
+              <Link href={`/posts/${i.slug}`} key={i.slug}>
+                <a>
+                  <strong>{i.data.title}</strong>
+                  <p>{i.data.content[0].text || ''}</p>
+                  <time>{i.first_publication_date}</time>
+                </a>
+              </Link>
+            );
+          })}
+        </div>
+      </main>
+    </>
+  );
 }
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient({});
-//   // const postsResponse = await prismic.getByType(TODO);
+export const getStaticProps = async () => {
+  const prismic = getPrismicClient({});
+  const postsResponse = await prismic.getByType('post');
 
-//   // TODO
-// };
+  const posts = postsResponse.results.map(i => ({
+    slug: i.uid,
+    uid: i.uid,
+    first_publication_date: new Date(
+      i.first_publication_date
+    ).toLocaleDateString('pt-BR', {
+      day: '2-digit',
+      month: 'long',
+      year: 'numeric',
+    }),
+    data: {
+      title: i.data.title,
+      subtitle: i.data.subtitle,
+      author: i.data.author,
+      banner: {},
+      content: i.data.content.map(i => i.heading[0]),
+    },
+  }));
+
+  return {
+    props: {
+      postsPagination: {
+        results: posts,
+      },
+    },
+  };
+};
